656e4ab51b4ccb1533787b602721ec107dc9d2c1
avneet-kaur <avneepannu@gmail.com>
2020-07-23 12:57:54 +0530

    Update urls.py
    changing include

--- mySite/urls.py
+++ mySite/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import include,path
+from django.urls import path
+from django.conf.urls import include
 from django.contrib import admin
 
 from django.contrib.auth.views import LoginView,LogoutView
